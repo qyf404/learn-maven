0eca2e03fee6b49a251cc0ea8cd429ac5ebf499d
飞车兔 <qyf404@gmail.com>
2015-09-23 16:12:41 +0800

    add App.class

---
+++ src/main/java/com/qyf404/learn/maven/App.java
@@ -0,0 +1,9 @@
+package com.qyf404.learn.maven;
+/**
+ * Created by qyfmac on 15/9/23.
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.printf("learn maven app.");
+    }
+}
